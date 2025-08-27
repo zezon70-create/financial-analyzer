6234ed207a5750dca95a7a8fc150215d7d3dba1c
zezon70-create <zezon70@gmail.com>
2025-08-27 14:35:12 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/financial-analyzer/', // ضع هنا اسم المستودع على GitHub
+})
