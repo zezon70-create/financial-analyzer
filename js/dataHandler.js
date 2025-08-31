6b9c657c7215d5e800bfeb4060fbd52e06f97670
zezon70-create <zezon70@gmail.com>
2025-08-31 14:11:09 +0300

    Update dataHandler.js

--- js/dataHandler.js
+++ js/dataHandler.js
@@ -1,1 +1,26 @@
-// Data handler placeholder
+let data = [];
+
+document.getElementById('dataForm')?.addEventListener('submit', function(e){
+    e.preventDefault();
+    let account = document.getElementById('accountName').value;
+    let debit = parseFloat(document.getElementById('debit').value);
+    let credit = parseFloat(document.getElementById('credit').value);
+    data.push({account, debit, credit});
+    renderTable();
+});
+
+document.getElementById('clearData')?.addEventListener('click', function(){
+    data = [];
+    renderTable();
+});
+
+function renderTable(){
+    let tableDiv = document.getElementById('dataTable');
+    if(!tableDiv) return;
+    let html = "<table border='1'><tr><th>الحساب</th><th>مدين</th><th>دائن</th></tr>";
+    data.forEach(d => {
+        html += `<tr><td>${d.account}</td><td>${d.debit}</td><td>${d.credit}</td></tr>`;
+    });
+    html += "</table>";
+    tableDiv.innerHTML = html;
+}
