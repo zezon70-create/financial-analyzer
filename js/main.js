c296c29aee995ecd39a20adfa1facc47d12d6bc9
zezon70-create <zezon70@gmail.com>
2025-08-31 13:39:02 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('lang-toggle')?.addEventListener('click', toggleLang);
-
-let currentLang = 'en';
-function toggleLang(){
-    currentLang = currentLang === 'en' ? 'ar' : 'en';
-    alert(`Language changed to ${currentLang}`);
-    // هنا ممكن نضيف التغيير الديناميكي لكل النصوص من ملفات i18n/ar.json و en.json
-}
