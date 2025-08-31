89c9d7bc0389d744d7328183b3a680d86c5c69ab
zezon70-create <zezon70@gmail.com>
2025-08-31 15:10:57 +0300

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,4 +1,4 @@
-function exportExcel(){ alert("سيتم تنفيذ تصدير Excel"); }
-function exportPDF(){ alert("سيتم تنفيذ تصدير PDF"); }
+function exportExcel(){ alert("سيتم التصدير إلى Excel"); }
+function exportPDF(){ alert("سيتم التصدير إلى PDF"); }
 document.getElementById('exportExcel')?.addEventListener('click', exportExcel);
 document.getElementById('exportPDF')?.addEventListener('click', exportPDF);
