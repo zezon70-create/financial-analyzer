8df6155b0a3be9696dbd495a8a67292d1d4fb46c
zezon70-create <zezon70@gmail.com>
2025-08-31 14:11:24 +0300

    Update charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,1 +1,15 @@
-// Charts placeholder
+let ctx = document.getElementById('financialChart');
+if(ctx){
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['الأرباح', 'النفقات', 'الأصول', 'الخصوم'],
+            datasets: [{
+                label: 'القيم المالية',
+                data: [120000, 90000, 250000, 150000],
+                backgroundColor: ['#ff7f50','#1e90ff','#32cd32','#ffa500']
+            }]
+        },
+        options: {responsive: true}
+    });
+}
