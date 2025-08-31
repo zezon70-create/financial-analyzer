b78cdaeb6dd7c73c90a0e33c13fefc46ee8c6151
zezon70-create <zezon70@gmail.com>
2025-08-31 15:10:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.getElementById('languageSwitch')?.addEventListener('change', function() {
+document.getElementById('languageSwitch')?.addEventListener('change', function(){
     let lang = this.value;
     alert("تم اختيار اللغة: " + lang);
 });
