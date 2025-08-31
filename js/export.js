608cbed796ba067bf4198a07980bbe095d1c1dbf
zezon70-create <zezon70@gmail.com>
2025-08-31 14:03:27 +0300

    Delete js directory

--- js/export.js
+++
@@ -1,3 +0,0 @@
-// Excel & PDF Export (محتوى أساسي placeholder)
-function exportToExcel(){ alert('Excel export'); }
-function exportToPDF(){ alert('PDF export'); }
--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('lang-toggle')?.addEventListener('click', toggleLang);
-let currentLang = 'en';
-function toggleLang(){
-    currentLang = currentLang === 'en' ? 'ar' : 'en';
-    alert(`Language changed to ${currentLang}`);
-}
