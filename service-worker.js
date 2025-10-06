1742f2b96fe0a091d7188d6f40aec1caebfa3f7f
zezon70-create <zezon70@gmail.com>
2025-10-06 10:30:23 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-
-self.addEventListener('install', function(e){ self.skipWaiting(); });
-self.addEventListener('activate', function(e){ console.log('SW activated'); });
-self.addEventListener('fetch', function(e){ /* basic offline fallback could be added */ });
